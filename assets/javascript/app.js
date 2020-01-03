031992332a1016f69d1f225fad46f8ae19683f89
Charlie <charliecotton04@gmail.com>
2020-01-03 04:38:39 -0500

    made changes to the index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,25 @@ var score = 0
 var recentQuestion = 0;
 var questions = [
   {
-    name: 'q1';
+    name: 'q1',
     title: "What type of pokemon is Charmander?",
     answer: ['water', 'fire', 'grass','electric'],
     correct: 0
   },
   {
-    
+    name:'q2',
     title: "What type of pokemon is Bulbasaur?",
     answer: ['water', 'fire', 'grass', 'electric'],
     correct: 1
   },
   {
-     title: "What type of pokemon is Squirtle?",
+      name:'q3',
+     title: "What type of pokemon is Venasaur?",
      answer:['water', 'fire','grass', 'electric'],
      correct:2 
     },
     {
+      name:'q4',
      title: "What type of pokemon is Pikachu?",
      answer:['water', 'fire','grass', 'electric'],
      correct: 3
@@ -32,7 +34,7 @@ var questions = [
         showQuestion();
        
     });
-
+$(.trivia)
 
 
 
@@ -47,7 +49,10 @@ var questions = [
         $('.trivia h2').text(question.title);
         $('.trivia ul').html('');
         for(var i=0; i<question.answers.length; i++){
-          $  
+          $('.trivia ul').append(<li id="+i+">${question.answers[i]}')"</li>);
+
+
+          )
         }
 
     }
