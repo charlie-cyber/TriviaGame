1e14d5be3ed9ec65a179ca9c0cea8d7f78ab5dff
Charlie <charliecotton04@gmail.com>
2020-01-09 09:07:46 -0500

    it works finally

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-var score = 0;
-var recentQuestion = 0;
 var triviaQuestions = [
   {
     question:
@@ -8,31 +6,36 @@ var triviaQuestions = [
       "What's up doc?",
       "Great horny toads!",
       "Ay carumba!",
-      "Suffering Succotash"
-    ],
-    correct: 0
+      "Suffering Succotash"],
+      answer: 0,
+       
+  
   },
   {
     question:
       " Known for his apparently bottomless stomach, dizzy demeanor, and guttural 'speech', what character is actually based on a quadruped of the southern hemisphere??",
     showAnswer: ["Taz", "Wild E. Coyote ", "Daffy", "Speedy Gonzalez"],
-    correct: 1
+    answer: 1,
+  
   },
   {
     question:
       "In which year did The Road Runner Show make its US television debut?",
     showAnswer: ["1987", "1966", "2001", "1995"],
-    correct: 2
+    answer: 2,
+  
   },
   {
     question:
       "What is the name of the female bunny that sometimes appears as the love interest for Bugs Bunny in Looney Tunes?",
     showAnswer: ["Penelope", "Babs", "Lola", "Petunia"],
-    correct: 3
+    answer: 3,
+   
   }
 ];
-var rightAnswer;
-var wrongAnswer;
+var recentQuestion;
+var correctAnswer;
+var incorrectAnswer;
 var notanswered;
 var time;
 var seconds;
@@ -45,88 +48,90 @@ var messages = {
   timeEnd: "That's All Folks",
   finished: "Woo-hoo!"
 };
-$("#startOBtn").on("click", function() {
+$('#startBtn').on('click', function() {
   $(this).hide();
   startTrivia();
 });
-$("#startOverBtn").on("click", function() {
+$('#startOverBtn').on('click', function() {
   $(this).hide();
   startTrivia();
 });
 
 function startTrivia() {
-  $("#finalMessage").empty();
-  $("#correctAnswers").empty();
-  $("#incorrectAnswers").empty();
-  $("#notanswered").empty();
-  currentQuestion = 0;
-  rightAnswer = 0;
-  wrongAnswer = 0;
+  console.log('startTrivia')
+  $('#finalMessage').empty();
+  $('#correctAnswers').empty();
+  $('#incorrectAnswers').empty();
+  $('#notanswered').empty();
+  recentQuestion = 0;
+  correctAnswer = 0;
+  incorrectAnswer = 0;
   notAnswered = 0;
   newQuestion();
 }
 function newQuestion() {
-  $("#message").empty();
-  $("#rightAnswer").empty();
+  $('#message').empty();
+  $('#correctAnswer').empty();
   answered = true;
-}
 
-$("recentQuestion").html(
-  "Question #" + (recentQuestion + 1) + "/" + triviaQuestions.length
-);
-$(".question").html(
-  "<h2>" + triviaQuestions[recentQuestion].question + "</h2>"
-);
+
+// sets up new Question
+  $('#recentQuestion').html('Question #'+(recentQuestion+1)+'/'+triviaQuestions.length);
+  $('.question').html('<h2>' + triviaQuestions[recentQuestion].question + '</h2>');
+  
 for (var i = 0; i < 4; i++) {
-  var options = $("<div>");
+  var options = $('<div>');
   options.text(triviaQuestions[recentQuestion].showAnswer[i]);
-  options.attr({ "data-index": i });
-  options.addClass("thisOption");
-  $(".answerList").append(options);
+  options.attr({ 'data-index': i });
+  options.addClass('thisOption');
+  $('.showAnswer').append(options);
 }
 countdown();
-$(".thisOption").on("click", function() {
-  userPick = $(this).data("index");
+$('.thisOption').on('click', function() {
+  userPick = $(this).data('index');
   clearInterval(time);
-  answerPage();
+  showAnswer();
 });
+
+}
+
 function countdown() {
   seconds = 20;
-  $("#timeLeft").html("<h3>TimeRemaining: " + seconds + "</h3>");
+  $('#timeLeft').html('<h3>TimeRemaining: ' + seconds + '</h3>');
   answered = true;
-  time = setInterval(showCountdown, 1000);
-}
+  time = setInterval(countdown, 1000);
 
-function displayCountdown() {
+}
+function displayCountdown(){
   seconds--;
-  $("#timeLeft").html("<h3>Time Remaining: " + seconds + "</h3>");
-  if (seconds < 1) {
-    clearInterval(time);
-    answered = false;
-    answerPage();
-  }
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+        showAnswer();
 }
-function checkAnswer() {
-  $("#recentQuestion").empty();
-  $(".thisOption").empty();
-  $(".question").empty();
-  var correctAnswerText =
-    triviaQuestions[recentQuestion].showAnswer[
-      triviaQuestions[recentQuestion].answer
-    ];
-  var correctAnswerIndex = triviaQuestions[recentQuestion].answer;
 
-  if (userPick == correctAnswerIndex && answered == true) {
-    rightAnswer++;
-    $("#message").html(messages.right);
+}
+
+function showAnswer() {
+	$('#recentQuestion').empty();
+	$('.thisOption').empty(); 
+  $('.question').empty();
+  
+  var correctAnswerText = triviaQuestions[recentQuestion].showAnswer[triviaQuestions[recentQuestion].answer];
+  var correctAnswerIndex = triviaQuestions[recentQuestion].answer;
+  
+if ((userPick == correctAnswerIndex) && (answered == true)){
+    correctAnswer++;
+    $('#message').html(messages.correct);
   } else if (userPick != correctAnswerIndex && (answered = true)) {
-    wrongAnswer++;
-    $("#message").html(messages.wrong);
-    $("#rightAnswer").html("The right answer was:" + rightAnswerText);
+    incorrectAnswer++;
+    $('#message').html(messages.incorrect);
+    $('#correctAnswer').html('The correct answer was:' + correctAnswerText);
   } else {
     notanswered++;
-    $("#message").html(messages.endTime);
-    $("#rightAnswer").html("The right answer was:" + rightAnswerText);
+    $('#message').html(messages.endTime);
+    $('#correctAnswer').html('The correct answer was:'+ correctAnswerText);
     answered = true;
   }
   if (recentQuestion == triviaQuestions.length - 1) {
@@ -137,3 +142,19 @@ function checkAnswer() {
     clearTimeout(recentQuestion);
   }
 }
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("incorrectAnswers: " + incorrectAnswer);
+	$('#notanswered').html("Notanswered: " + notanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
+  
+
